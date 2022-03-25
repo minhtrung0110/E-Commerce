84818201a97d6a24a0ae3f17520ef7c0c39f2fcd
Minh Trung <nguyenducminhtrung0044@gmail.com>
2022-03-25 14:48:16 +0700

    Init project

---
+++ shop-ecommerce/resources/views/admin/login.blade.php
@@ -0,0 +1,2 @@
+<h1>Hello Loigin</h1>
+{{$title}}
--- shop-ecommerce/routes/web.php
+++ shop-ecommerce/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\User\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+/*Admin*/
+Route::prefix('admin')->group(function(){
+  Route::get('/user/login',[LoginController::class,'index'])->name('login');
 });
