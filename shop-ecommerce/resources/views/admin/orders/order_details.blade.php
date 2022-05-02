a1f01ab5c9963143dbeb1d48014755cc61667218
ADMIN <you@example.com>
2022-05-02 07:42:46 +0700

    nghia

--- shop-ecommerce/resources/views/admin/orders/order_details.blade.php
+++ shop-ecommerce/resources/views/admin/orders/order_details.blade.php
@@ -11,19 +11,35 @@
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
       {{-- code --}}
-      <a href="/admin/orders/list" class="btn btn-success">Quay lại</a>
-    <div class="text-center">
-        <h1>Thông tin khách hàng</h1>
-        <p>Tên: {{$orderItems[0]['first_name'].' '.$orderItems[0]['last_name']}}</p>
-        <p>Số điện thoạt:{{$orderItems[0]['phone']}} </p>
-        <p>Email:{{$orderItems[0]['email']}} </p>
-        <p>Địa chỉ:{{$orderItems[0]['address']}} </p>
-        <p>Ngày đặt :{{$orderItems[0]['created_at']}}</p>
+    
+      <div class="card card-success  " style="padding:1em 8em;min-height: ">
+        
+        <div class="card-header">
+          <h3 class="card-title">Thông tin khách hàng</h3>
 
-    </div>
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+              <i class="fas fa-minus"></i>
+            </button>
+            
+          </div>
+        </div>
+      
+        <div class="card-body">
+          <div class="">
+        
+              <p>TÊN : {{$orderItems[0]['first_name'].' '.$orderItems[0]['last_name']}}</p>
+              <p>SỐ ĐIỆN THOẠT :{{$orderItems[0]['phone']}} </p>
+              <p>EMAIL :{{$orderItems[0]['email']}} </p>
+              <p>ĐỊA CHỈ :{{$orderItems[0]['address']}} </p>
+              <p>NGÀY ĐẶT :{{$orderItems[0]['created_at']}}</p>
+
+          </div>
+        </div>
+      </div>
     <div class="text-center"><h1>Chi tiết hóa đơn</h1></div>
-    <table class="table">
-      <thead>
+    <table class="table ">
+      <thead class="thead-dark">
         <tr>
           <th style="width:50px" scope="col">STT</th>
           <th scope="col">Tên sản phẩm</th>
@@ -50,9 +66,32 @@
         </tr>
             
         @endforeach
-
+        
       
       </tbody>
     </table>
   </div>
+  {{-- <button type="button" class="btn btn-primary launch" data-toggle="modal" data-target="#staticBackdrop"> <i class="fa fa-info"></i> Get information
+  </button>
+  <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog">
+          <div class="modal-content">
+              <div class="modal-body ">
+                  <div class="text-right"> <i class="fa fa-close close" data-dismiss="modal"></i> </div>
+                  <div class="px-4 py-5">
+                      <h5 class="text-uppercase">Jonathan Adler</h5>
+                      <h4 class="mt-5 theme-color mb-5">Thanks for your order</h4> <span class="theme-color">Payment Summary</span>
+                      <div class="mb-3">
+                          <hr class="new1">
+                      </div>
+                      <div class="d-flex justify-content-between"> <span class="font-weight-bold">Ether Chair(Qty:1)</span> <span class="text-muted">$1750.00</span> </div>
+                      <div class="d-flex justify-content-between"> <small>Shipping</small> <small>$175.00</small> </div>
+                      <div class="d-flex justify-content-between"> <small>Tax</small> <small>$200.00</small> </div>
+                      <div class="d-flex justify-content-between mt-3"> <span class="font-weight-bold">Total</span> <span class="font-weight-bold theme-color">$2125.00</span> </div>
+                      <div class="text-center mt-5"> <button class="btn btn-primary">Track your order</button> </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div> --}}
 @endsection
